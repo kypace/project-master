bde15b5dc5578f9f9a4d002a4233cc7f81a55d77
github_kypace <kevin_yoo90@hotmail.com>
2018-05-10 14:30:21 -0700

    themoviedbjs updated ui

--- themoviedb.js
+++ themoviedb.js
@@ -28,6 +28,64 @@ var search = (query) => {
     });
 }
 
+var peopleSearch = (query) => {
+    return new Promise((resolve, reject) => {
+        request({
+            url: 'https://api.themoviedb.org/3/search/person?api_key=' + key + '&query=' + encodeURIComponent(query),
+            json: true
+        }, (error, response, body) => {
+            if (error) {
+                reject('Cannot connect to TheMovieDB');
+            } else if (body.total_results < 1) {
+                reject('No results found for query');
+            } else {
+                resolve(
+                    body.results
+                );
+            }
+        });
+    });
+}
+
+var actorCreditSearch = (personid) => {
+    return new Promise((resolve, reject) => {
+        request({
+            url: 'https://api.themoviedb.org/3/person/' + personid + '/movie_credits?api_key=' + key + '&query=' + encodeURIComponent(query),
+            json: true
+        }, (error, response, body) => {
+            if (error) {
+                reject('Cannot connect to TheMovieDB');
+            } else if (body.cast.length < 1) {
+                reject('No results found for query');
+            } else {
+                resolve(
+                    body.cast
+                );
+            }
+        });
+    });
+}
+
+var directorCreditSearch = (personid) => {
+    return new Promise((resolve, reject) => {
+        request({
+            url: 'https://api.themoviedb.org/3/person/' + personid + '/movie_credits?api_key=' + key + '&query=' + encodeURIComponent(query),
+            json: true
+        }, (error, response, body) => {
+            if (error) {
+                reject('Cannot connect to TheMovieDB');
+            } else if (body.crew.length < 1) {
+                reject('No results found for query');
+            } else {
+                resolve(
+                    body.crew
+                );
+            }
+        });
+    });
+}
+
+
 /**
  * This function reads the results of the search to the console log
  */
@@ -52,19 +110,19 @@ var parseResults = (results) => {
     var parsed = "";
     for (var i = 0; i < results.length; i++) {
         var overview = results[i].overview;
-        if (overview.length > 600)
-            overview = overview.substring(0, 600) + "..";
+        if (overview.length > 250)
+            overview = overview.substring(0, 250) + "..";
         parsed += `
-        <div style='background-color:#FFFCF8; width:100%; height:20%; text-align:left; border-top:1px solid black; '>
-            <img src='http://image.tmdb.org/t/p/w92/${results[i].poster_path}' style='left=1vw; margin:5px; height:90%; vertical-align: top; display: inline; float: left'/>
-            <div style='width:100%; height:10%; vertical-align: top; display: inline'>
+        <div class='border border-light p-3 mb-3 col-3 bg-dark'>
+            <img src='http://image.tmdb.org/t/p/w92/${results[i].poster_path}' class='mPoster'/>
+            <div class='p-3 mb-2 mTextBox text-light text-left content'>
                 <strong>Title</strong>: ${results[i].title}<br>
                 <strong>Overview</strong>: ${overview}<br>
                 <strong>Release Date</strong>: ${results[i].release_date}<br>
                 <form action="/favorites" enctype="application/json" method="post">
                     <input id= "favIndex" name="favIndex" type="hidden" value=${i} />
                     <input id= "favPush" name="favPush" type="hidden" value="yes" />
-                    <input id="Favorite" action="/favorites" type="submit" value="Favorite" />
+                    <input class="fButton btn btn-secondary" id="Favorite" action="/favorites" type="submit" value="Favorite" />
                 </form>
             </div>
         </div>`;
@@ -86,19 +144,19 @@ var generateFavorites = (favorites) => {
     }
     for(var i = 0; i < favorites.length; i++) {
         var overview = favorites[i].overview;
-        if (overview.length > 600)
-            overview = overview.substring(0, 600) + "..";
+        if (overview.length > 250)
+            overview = overview.substring(0, 250) + "..";
         generated += `
-        <div style='background-color:#FFFCF8; width:100%; height:20%; text-align:left; border-top:1px solid black; '>
-            <img src='http://image.tmdb.org/t/p/w92/${favorites[i].poster_path}' style='left=1vw; margin:5px; height:90%; vertical-align: top; display: inline; float: left'/>
-            <div style='width:100%; height:10%; vertical-align: top; display: inline'>
+        <div class='border border-light p-3 mb-3 col-3 bg-dark'>
+            <img src='http://image.tmdb.org/t/p/w92/${favorites[i].poster_path}' class='mPoster'/>
+            <div class='p-3 mb-2 mTextBox text-light text-left content'>
                 <strong>Title</strong>: ${favorites[i].title}<br>
                 <strong>Overview</strong>: ${overview}<br>
                 <strong>Release Date</strong>: ${favorites[i].release_date}<br>
                 <form action="/favorites" enctype="application/json" method="post">
                     <input id= "favIndex" name="favIndex" type="hidden" value=${i} />
                     <input id= "favPush" name="favPush" type="hidden" value="no" />
-                    <input id="Unfavorite" action="/favorites" type="submit" value="Unfavorite" />
+                    <input class="fButton btn btn-secondary" id="Unfavorite" action="/favorites" type="submit" value="Unfavorite" />
                 </form>
             </div>
         </div>`;
@@ -106,9 +164,60 @@ var generateFavorites = (favorites) => {
     return generated;
 }
 
+var generatePeople = (results) => {
+    var parsed = "";
+    for (var i = 0; i < results.length; i++) {
+        parsed += `
+        <div style='background-color:#FFFCF8; width:100%; height:20%; text-align:left; border-top:1px solid black; '>
+            <img src='http://image.tmdb.org/t/p/w92/${results[i].profile_path}' style='left=1vw; margin:5px; height:90%; vertical-align: top; display: inline; float: left'/>
+            <div style='width:100%; height:10%; vertical-align: top; display: inline'>
+                <strong>Name</strong>: ${results[i].name}<br>
+            </div>
+        </div>`;
+    }
+    return parsed;
+}
+
+var sortReleaseDescending = (results) => {
+    var max = results.length;
+    var sorted = [];
+    var bigindex = 0;
+    for (var i = 0; i < max; i++) {
+        for (var j = 0; j < results.length; j++) {
+            if (results[j] > results[bigindex])
+                bigindex = j;
+        }
+        sorted.push(results[bigindex])
+        results.splice(bigindex, 1)
+    }
+    return sorted;
+}
+
+var sortReleaseAscending = (results) => {
+    var max = results.length;
+    var sorted = [];
+    var bigindex = 0;
+    for (var i = 0; i < max; i++) {
+        for (var j = 0; j < results.length; j++) {
+            if (results[j] < results[bigindex])
+                bigindex = j;
+        }
+        sorted.push(results[bigindex])
+        results.splice(bigindex, 1)
+    }
+    return sorted;
+}
+
 module.exports = {
     search,
+    peopleSearch,
+    actorCreditSearch,
+    directorCreditSearch,
     readResults,
     parseResults,
-    generateFavorites
+    generateFavorites,
+    generatePeople,
+    sortReleaseDescending,
+    sortReleaseAscending
 };
+
