551d5a4fb3957133741ae80e367c0db25c29d0b2
Seungil Park <seungilpark88@gmail.com>
2018-05-15 19:58:05 -0700

    bootstraped recommendations.hbs

--- tastedive.js
+++ tastedive.js
@@ -58,9 +58,9 @@ var parseRecommendations = (reclist) => {
         if (teaser.length > 600)
             teaser = teaser.substring(0, 600) + "..";
         generated += `
-        <div style='background-color:#FFFCF8; width:100%; height:20%; text-align:left; border-top:1px solid black;'>
-            <iframe src='${reclist[i].yUrl}' allowFullScreen style='left=1vw; margin:5px; vertical-align: top; height:85%; display: inline; float: left'></iframe> 
-            <div style='width:100%; height:10%; vertical-align: top; display: inline'>
+        <div class="col-xs-12 col-md-6 col-lg-4 recDiv bg-light text-dark">
+            <iframe src='${reclist[i].yUrl}' allowFullScreen class ="mr-3 embed-responsive-item mTrailer"></iframe> 
+            <div class="m-3 text-dark">
                 <strong>Title</strong>: ${reclist[i].Name}<br>
                 <strong>Overview</strong>: ${teaser}<br>
             </div>
